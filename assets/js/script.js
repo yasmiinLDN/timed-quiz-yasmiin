e6a25e4ff7ed885e1b62eb45954b9f6ede9297a5
Yasmin Ibrahim <yasmin@Yasmins-MacBook-Air.local>
2024-01-03 10:40:14 +0000

    added pseudocode from before

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,41 @@
+// PSEUDOCODE FOR QUIZ
+
+// Set of questions --> array of objects
+// Each question needs the following:
+  // Question text
+  // Set of answers
+  // Which answer is correct
+
+// Landing page:
+  // Explanation of the quiz
+  // Start button
+
+// Click the start button:
+  // Landing page goes away
+  // Timer starts (NEED a conditional in there to check whether the answer was correct, which means you need to track whether the answer was correct).
+  // The first question appears (with its answers)
+
+// For each question:
+  // User clicks an answer
+  // Their choice is compared to the correct answer as stored in the question's object
+  // If correct, tell them
+  // If incorrect, tell them AND subtract time from the timer
+  // Optional: play a sound for correct or incorrect
+  // Either way, the question disappears after a few seconds and the next question appears
+
+// After the last question:
+  // Timer stops
+  // Question disappears
+  // Form appears for user to enter their initials
+  // Display their score
+
+// User submits form
+  // Initials and score get stored in local storage
+  // User is taken to the high scores page
+  // High scores are listed, sorted highest to lowest
+  // User has option to take the quiz again
+  
+
 /// starter code (29th Dec) -- test in index !
 
 
